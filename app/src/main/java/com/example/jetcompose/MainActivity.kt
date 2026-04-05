5585521cd36570242e0fe938952578684e80c86d
Aryan Raj <ansaryanpratap14@gmail.com>
2026-04-06 00:43:33 +0530

    Adding new file

--- app/src/main/java/com/example/jetcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetcompose/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.jetcompose.JetpackCompose.CardExample2
-import com.example.jetcompose.JetpackCompose.RememberSaveableExample
-import com.example.jetcompose.JetpackCompose.StateManagementExample
+import com.example.jetcompose.JetpackCompose.LazyColumnExample
+import com.example.jetcompose.JetpackCompose.NestedLazyExample
+import com.example.jetcompose.JetpackCompose.TypesOfItem
 import com.example.jetcompose.JetpackCompose.c10_navigation.LoginScreenUI
 import com.example.jetcompose.ui.theme.JetComposeTheme
-import com.example.jetpackcomposemissionandroid.jetpackcompose.AlertDialogExample
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +18,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetComposeTheme {
 
-                LoginScreenUI()
+                TypesOfItem()
+
 
 
             }
