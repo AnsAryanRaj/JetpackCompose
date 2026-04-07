fc24716069b4bea3f31d42911cb7ae8fddf2850f
Aryan Raj <ansaryanpratap14@gmail.com>
2026-04-07 22:40:47 +0530

    c13 Navigation bar files

--- app/src/main/java/com/example/jetcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetcompose/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.jetcompose.JetpackCompose.LazyColumnExample
-import com.example.jetcompose.JetpackCompose.NestedLazyExample
-import com.example.jetcompose.JetpackCompose.TypesOfItem
-import com.example.jetcompose.JetpackCompose.c10_navigation.LoginScreenUI
+import com.example.jetcompose.JetpackCompose.c13_navigation_bar.NavBarHomeScreen
+import com.example.jetcompose.JetpackCompose.c13_navigation_bar.NavBarNavigation
 import com.example.jetcompose.ui.theme.JetComposeTheme
-import com.example.jetpackcomposemissionandroid.jetpackcompose.c12_scaffold.MyScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +16,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetComposeTheme {
 
+                NavBarNavigation()
 
-                MyScreen()
+
+
+
+
+
+
 
 
 
